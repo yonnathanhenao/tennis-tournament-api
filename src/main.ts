237e57dcb9933317f7e5ce6b2ef5ab18a7dbda42
Tan <dev.yhenao@gmail.com>
2023-11-24 17:30:24 -0500

    feat(app): add env port

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 
+const port: string | number = process.env.PORT || 3000;
+
 async function bootstrap() {
   const app: INestApplication = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
